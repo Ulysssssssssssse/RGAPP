c452c0b8d4efb51c8ea82d3e1740aba37ad20d1c
Leonicolash <nicolas.hing@gmail.com>
2022-12-19 16:20:42 +0100

    footer.php

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer>           
+        <a href="CGU.html">Mentions légales</a>
+        <a href="mailto:ridegreencontact@gmail.com" title="ridegreencontact@gmail.com">Contact</a>
+        <a href="FAQ.html">FAQ</a>
+</footer>
