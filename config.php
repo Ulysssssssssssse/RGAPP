c47906e7e1032f3211e86773aedabf73b42f2f75
Ulysssssssssssse <ulysse.musard@hotmail.com>
2023-01-06 21:52:19 +0100

    Revert "correction config"
    This reverts commit 249f317dfaecf5b5c2f9c1f2a85b260ecf94a27b.

--- config.php
+++ config.php
@@ -2,21 +2,11 @@
 
 try
 {
-    $bdd = new PDO('mysql:host=localhost; dbname=isepbike; charset=utf8', 'root', '');}
-    
-    catch(Exception $e)
+    $bdd = new mysqli('localhost; dbname=isepbike; charset=utf8', 'root', '');
+}catch(Exception $e)
 
 {
     die('Erreur' . $e->getMessage());
 }
 
 ?>
-
-<?php
-  $host       = "<localhost>";
-  $username   = "<root>";
-  $password   = "<1234>";
-  $dsn        = "mysql:host=$host;dbname=isepbike";
-  $options    = array(
-                  PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-                );
