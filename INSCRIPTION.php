7b7fc356d7b7a71b89b6bf0d879ec25eda53868b
Leonicolash <nicolas.hing@gmail.com>
2023-01-09 13:38:58 +0100

    évitez de sur-indenter, ecran

--- INSCRIPTION.php
+++ INSCRIPTION.php
@@ -11,111 +11,112 @@
     <title>rGapp</title>
 </head>
 
-            <body id="inscription">
-            <header>
-            <div class="ligne1">
-            <a href="PRESENTATION.html"><img src="images/Ride Green logo.png" alt="logo Ridegreen" id="logo" /></a>
-            <h1 class="titre">rGapp</h1>
-            <a href="PRESENTATION.html"><img src="images/Isepbike logo.png" alt="logo Ridegreen" id="logo" class="logoisepbike"/></a>
-            <ul class="navbar">
-                <li >
-                  <div href="#" class="logomenu"><img src="images/menu.png"></div>
-                  <ul>
-                    <li><a href="Présentation.html">Présentation</a></li>
-                    <li><a href="connexion.php">Connexion</a></li>
-                    <li><a href="inscription.php">Inscription</a></li>
-                  </ul>
-                </li>
-            </ul>
-            </div>
-            <div class="ligne2">
-                <a href="PRESENTATION.html">Présentation</a>
-                <a href="connexion.php">Connexion</a>
-                <a href="INSCRIPTION.php">Inscription</a>
-            </div>
-            <script src="headerresponsive.js"></script>    
+<body id="inscription">
 
-            </header>
-            <?php
-            if(isset($_GET['reg_err']))
-            {
-                $err = htmlspecialchars($_GET['reg_err']);
-                switch($err)
-                {
-                    case 'success':
+<header>
+<div class="ligne1">
+<a href="PRESENTATION.html"><img src="images/Ride Green logo.png" alt="logo Ridegreen" id="logo" /></a>
+<h1 class="titre">rGapp</h1>
+<a href="PRESENTATION.html"><img src="images/Isepbike logo.png" alt="logo Ridegreen" id="logo" class="logoisepbike"/></a>
+<ul class="navbar">
+    <li >
+        <div href="#" class="logomenu"><img src="images/menu.png"></div>
+        <ul>
+        <li><a href="Présentation.html">Présentation</a></li>
+        <li><a href="connexion.php">Connexion</a></li>
+        <li><a href="inscription.php">Inscription</a></li>
+        </ul>
+    </li>
+</ul>
+</div>
+<div class="ligne2">
+    <a href="PRESENTATION.html">Présentation</a>
+    <a href="connexion.php">Connexion</a>
+    <a href="INSCRIPTION.php">Inscription</a>
+</div>
+<script src="headerresponsive.js"></script>    
+</header>
+
+    <?php
+    if(isset($_GET['reg_err']))
+    {
+        $err = htmlspecialchars($_GET['reg_err']);
+        switch($err)
+        {
+            case 'success':
+    ?>
+                    <div class="alert alert-success">
+                        <strong>Succès </strong> inscription reussie ! 
+                    </div>
+                    <?php
+                    break;
+
+            case 'password':
+                    ?>
+                    <div class="alert alert-danger">
+                        <strong> Erreur </strong> mot de passe différents !   <!-- Test OK-->
+                    </div>
+                    <?php
+                    break;
+
+            case 'email':
+                    ?>
+                    <div class="alert alert-danger">
+                        <strong> Erreur </strong> email non valide ! <!-- Test OK, il faut modifier l'attribut "mail" en "text" pour pouvoir afficher l'erreur -->
+                    </div>
+                    <?php
+                    break;
+                            
+            case 'email_length':
+                    ?>
+                    <div class="alert alert-danger">
+                        <strong> Erreur </strong> email trop long ! <!-- Test OK, inspecter et transformer 'mail' en 'text' pour afficher l'erreur-->
+                    </div>
+                    <?php
+                    break;
+
+            case 'pseudo_length':
+                    ?>
+                    <div class="alert alert-danger">
+                        <strong> Erreur </strong> pseudo trop long ! <!-- Test OK-->
+                    </div>
+                    <?php
+                    break;
+
+
+            case 'already':
+                    ?>
+                    <div class="alert alert-danger">
+                        <strong>Erreur </strong> compte deja existant ! <!-- Test OK-->
+                    </div>
+                    <?php
+                    break;
+
+        }
+    }
+
         ?>
-                            <div class="alert alert-success">
-                                <strong>Succès </strong> inscription reussie ! 
-                            </div>
-                            <?php
-                            break;
 
-                    case 'password':
-                            ?>
-                            <div class="alert alert-danger">
-                                <strong> Erreur </strong> mot de passe différents !   <!-- Test OK-->
-                            </div>
-                            <?php
-                            break;
-
-                    case 'email':
-                            ?>
-                            <div class="alert alert-danger">
-                                <strong> Erreur </strong> email non valide ! <!-- Test OK, il faut modifier l'attribut "mail" en "text" pour pouvoir afficher l'erreur -->
-                            </div>
-                            <?php
-                            break;
-                                    
-                    case 'email_length':
-                            ?>
-                            <div class="alert alert-danger">
-                                <strong> Erreur </strong> email trop long ! <!-- Test OK, inspecter et transformer 'mail' en 'text' pour afficher l'erreur-->
-                            </div>
-                            <?php
-                            break;
-
-                    case 'pseudo_length':
-                            ?>
-                            <div class="alert alert-danger">
-                                <strong> Erreur </strong> pseudo trop long ! <!-- Test OK-->
-                            </div>
-                            <?php
-                            break;
-
-
-                    case 'already':
-                            ?>
-                            <div class="alert alert-danger">
-                                <strong>Erreur </strong> compte deja existant ! <!-- Test OK-->
-                            </div>
-                            <?php
-                            break;
+<form action="inscription_traitement.php" method="post">
+    <div class="inscription">
+    <h1> Inscription </h1>
+        <input class="barre" type="text" name="pseudo" placeholder="Pseudo" required="required" autocomplete="off">
+    <br><br>
+        <input class="barre" type="email" name="email"  placeholder="Email" required="required" autocomplete="off">
+    <br><br>
+        <input class="barre" type="password" name="password" placeholder="Mot de passe" required="required" autocomplete="off">
+    <br><br>
+        <input class="barre" type="password" name="password_retype" placeholder="Retapez le mot de passe" required="required" autocomplete="off">
+    <br>
+    <div style="color: black;width: 7cm;">
+        <input type="checkbox" id="CGU" name="CGU" value="CGU" required="required">
+        En cochant la case, vous acceptez nos <a href="CGU.html">termes et conditions d'utilisation</a>
+    </div>
+    <br>
+    <button type = "submit" class="btn btn-primary btn-block">Inscription</button>
     
-                }
-            }
-
-                            ?>
-
-        <form action="inscription_traitement.php" method="post">
-            <div class="inscription">
-            <h1> Inscription </h1>
-                <input class="barre" type="text" name="pseudo" placeholder="Pseudo" required="required" autocomplete="off">
-            <br><br>
-                <input class="barre" type="email" name="email"  placeholder="Email" required="required" autocomplete="off">
-            <br><br>
-                <input class="barre" type="password" name="password" placeholder="Mot de passe" required="required" autocomplete="off">
-            <br><br>
-                <input class="barre" type="password" name="password_retype" placeholder="Retapez le mot de passe" required="required" autocomplete="off">
-            <br>
-            <div style="color: black;width: 7cm;">
-                <input type="checkbox" id="CGU" name="CGU" value="CGU" required="required">
-                En cochant la case, vous acceptez nos <a href="CGU.html">termes et conditions d'utilisation</a>
-            </div>
-            <br>
-            <button type = "submit" class="btn btn-primary btn-block">Inscription</button>
-            
-            </div>
-        </form>
+    </div>
+</form>
 
 
 <?php include('footer.php'); ?>
