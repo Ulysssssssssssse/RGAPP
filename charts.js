a9e776322fb5f7d79fc653430164294386f70d3d
Leonicolash <nicolas.hing@gmail.com>
2023-01-13 09:56:23 +0100

    changement titre graphique

--- charts.js
+++ charts.js
@@ -5,51 +5,51 @@ google.charts.setOnLoadCallback(drawTable);
 
 function drawChart() {
     var data = google.visualization.arrayToDataTable([
-    ['Time', 'CO2'],
-    ['11h',  880],
-    ['11h15',865],
-    ['11h30',775],
-    ['11h45', 800],
-    ['12h00', 815]
+      ['Time', 'CO2'],
+      ['11h',  880],
+      ['11h15',865],
+      ['11h30',775],
+      ['11h45', 800],
+      ['12h00', 815]
     ]);
     var options = {
-    title: 'CO2',
-    curveType: 'function',
-    legend:'none'
+      title: 'CO2 (ppm)',
+      curveType: 'function',
+      legend:'none'
     };
     var chart = new google.visualization.LineChart(document.getElementById('chartCO2'));
     chart.draw(data, options);
 
 
     var data = google.visualization.arrayToDataTable([
-    ['Time', 'BPM'],
-    ['11h',  105],
-    ['11h15',145],
-    ['11h30',155],
-    ['11h45', 160],
-    ['12h00', 140]
+      ['Time', 'BPM'],
+      ['11h',  105],
+      ['11h15',145],
+      ['11h30',155],
+      ['11h45', 160],
+      ['12h00', 140]
     ]);
     var options = {
-    title: 'BPM',
-    curveType: 'function',
-    legend:'none'
+      title: 'Fréquence cardiaque (BPM)',
+      curveType: 'function',
+      legend:'none'
     };
     var chart2 = new google.visualization.LineChart(document.getElementById('chartBPM'));
     chart2.draw(data, options);
     
 
     var data = google.visualization.arrayToDataTable([
-    ['Time', '°C'],
-    ['11h',  37.5],
-    ['11h15',37.6],
-    ['11h30',37.8],
-    ['11h45', 37.7],
-    ['12h00', 37.5]
+      ['Time', '°C'],
+      ['11h',  37.5],
+      ['11h15',37.6],
+      ['11h30',37.8],
+      ['11h45', 37.7],
+      ['12h00', 37.5]
     ]);
     var options = {
-    title: 'Température corporelle',
-    curveType: 'function',
-    legend:'none'
+      title: 'Température corporelle (°C)',
+      curveType: 'function',
+      legend:'none'
     };
     var chart3 = new google.visualization.LineChart(document.getElementById('chart°C'));
     chart3.draw(data, options);
@@ -57,17 +57,17 @@ function drawChart() {
 
 
     var data = google.visualization.arrayToDataTable([
-    ['Time', 'dB'],
-    ['11h',  85],
-    ['11h15',65],
-    ['11h30',55],
-    ['11h45', 58],
-    ['12h00', 52]
+      ['Time', 'dB'],
+      ['11h',  85],
+      ['11h15',65],
+      ['11h30',55],
+      ['11h45', 58],
+      ['12h00', 52]
     ]);
     var options = {
-    title: 'Pollution sonore',
-    curveType: 'function',
-    legend:'none'
+      title: 'Pollution sonore (dB)',
+      curveType: 'function',
+      legend:'none'
     };
 
     var chart4 = new google.visualization.LineChart(document.getElementById('chartdB'));
