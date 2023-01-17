7467fff0f81632dafc90f899482fad1832d99e58
Ulysssssssssssse <ulysse.musard@hotmail.com>
2023-01-17 15:19:17 +0100

    test lightsail

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 try
 {
-    $bdd = new PDO('mysql:host=localhost; dbname=isepbike; charset=utf8', 'root', '');}
+    $bdd = new PDO('mysql:host=localhost; dbname=isepbike; charset=utf8', 'lightsail', 'lightsail');}
     
     catch(Exception $e)
 
