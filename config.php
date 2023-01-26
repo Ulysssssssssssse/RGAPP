98475a1187120876988304b50abc11f50cf5df9a
Ulysssssssssssse <ulysse.musard@hotmail.com>
2023-01-26 10:32:14 +0100

    back

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php 
 try
 {
-    $bdd = new PDO('mysql:host=localhost; dbname=isepbike; charset=utf8', 'lightsail', 'lightsail');}
+    $bdd = new PDO('mysql:host=localhost; dbname=isepbike; charset=utf8', 'root', '');}
     
     catch(Exception $e)
 {
