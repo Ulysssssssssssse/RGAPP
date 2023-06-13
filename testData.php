f73f6201f160d7b9da24993c6bacd72821484320
Amine1102 <amine.laimeche@gmail.com>
2023-06-13 10:48:00 +0200

    Affichage data

--- testData.php
+++ testData.php
@@ -1,4 +1,36 @@
 <?php
+  session_start();
+  if(!isset($_SESSION['user'])){
+      header('location:connexion.php');
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css" />
+        <link rel="stylesheet" href="style_accueil.css" />
+        <link rel="stylesheet" href="style_menudéroulant.css" />
+        <title>Accueil</title>
+        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+        <script type="text/javascript" src="charts.js"></script>
+</head>
+
+<body id="accueil">
+
+<?php include('header.php'); ?>
+    
+<h3> Bonjour <?php echo $_SESSION['user']; ?></h3>
+
+
+
+
+
+
+
+<?php
 
 
 
